538857a79d5946601bddaa2ff36c1db662b1c910
Abu1Hussain <alooyjamrawy@gmail.com>
2026-09-03 15:22:14 +0300

    last

---
+++ go.mod
@@ -0,0 +1,3 @@
+module goreloaded
+
+go 1.25.6
---
+++ main.go
@@ -0,0 +1,171 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	// case: hello OR Hi , ...
+	// the user gives exactly one argument in the terminal
+	if len(os.Args) == 2 {
+
+		// send the users input to the AsciiArt function
+		art, err := AsciiArt(os.Args[1])
+
+		// if there is an error stop the program
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		// print the generated ASCII art in the terminal
+		fmt.Print(art)
+
+		// also write the generated ASCII art to output.txt
+		err = os.WriteFile("output.txt", []byte(art), 0644)
+
+		// if the output file cannot be written -> stop the program
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		return
+	}
+
+	// case 2 the user has written input/output file
+	if len(os.Args) == 3 {
+
+		// input file name
+		inputFile := os.Args[1]
+
+		// output file name
+		outputFile := os.Args[2]
+
+		// Read all the data from the input file
+		data, err := os.ReadFile(inputFile)
+
+		// if the input file cannot be read -> stop the program
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		// convert the data from []byte to string
+		// and send it to the ASCII-art function
+		art, err := AsciiArt(string(data))
+
+		// if error when converting
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		// write the changes into the output file
+		err = os.WriteFile(outputFile, []byte(art), 0644)
+
+		// if the output file cannot be written -> stop the program
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		// also print the generated ASCII art in the terminal
+		fmt.Print(art)
+
+		return
+	}
+
+	// case if the user has entered an invalid input
+	fmt.Println("invalid input")
+}
+
+func AsciiArt(input string) (string, error) {
+
+	// this to replace the input in: "Hello\nWorld" =>
+	// "Hello
+	// World"
+	input = strings.ReplaceAll(input, `\n`, "\n")
+
+	// to handle the quote case: '\!" #$%&'"'"'()*+,-./'
+	// it will check for the start and ending
+	if len(input) >= 2 && ((strings.HasPrefix(input, "'") && strings.HasSuffix(input, "'")) ||
+		(strings.HasPrefix(input, `"`) && strings.HasSuffix(input, `"`))) {
+		input = input[1 : len(input)-1]
+	}
+
+	// will handle: '\!" #$%&'"'"'()*+,-./' , this: '"'"'
+	input = strings.ReplaceAll(input, `'"'"'`, "'")
+
+	// to replace the following: \" with only "
+	input = strings.ReplaceAll(input, `\"`, `"`)
+
+	// read from standard.txt file
+	fontBytes, err := os.ReadFile("standard.txt")
+
+	// if standard.txt cannot be opened or read -> return error
+	if err != nil {
+		return "", err
+	}
+
+	// handle a new line
+	fontContent := strings.ReplaceAll(string(fontBytes), "\r\n", "\n")
+
+	// split each line in standard.txt
+	fontLines := strings.Split(fontContent, "\n")
+
+	// for the output
+	var out strings.Builder
+
+	// split the input into lines
+	lines := strings.Split(input, "\n")
+
+	// process each line
+	for _, line := range lines {
+
+		// handle each empty line
+		if line == "" {
+
+			// add a newline
+			out.WriteByte('\n')
+
+			// skip the ASCII-art function for this line
+			continue
+		}
+
+		// process each row in the file
+		// each character has a height of 8 rows
+		for row := 0; row < 8; row++ {
+
+			// convert the word into individual characters
+			for j, char := range line {
+
+				// check if the character is supported
+				if char < ' ' || char > '~' {
+					return "", fmt.Errorf("invalid input: unsupported character")
+				}
+
+				// find the character in standard.txt
+				index := row + (int(char-' ') * 9) + 1
+
+				// add it to the output
+				out.WriteString(fontLines[index])
+
+				// add space between characters
+				if j != len([]rune(line))-1 {
+					out.WriteByte(' ')
+				}
+			}
+
+			// move to the next row
+			out.WriteByte('\n')
+		}
+	}
+
+	// convert strings.Builder into a string
+	// nil means there was no error
+	return out.String(), nil
+}
